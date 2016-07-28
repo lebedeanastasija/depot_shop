ed718fcf1fb8bab45446785477a9db4596fef6a5
lebedeanastasija <lebedeanastasija@yandex.by>
2016-07-28 14:53:36 +0300

    28-07-16--validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,68 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+  test "the truth" do
+    assert true
+  end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+    product = Product.new(title: "my_title",
+                           description: "my_book",
+                           image_url: "my_img.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be equal to or greater than 0.01"],
+        product.errors[:price]
+    product.price = 0
+
+    assert product.invalid?
+    assert_equal ["must be equal to or greater than 0.01"],
+        product.errors[:price]
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: "my_title",
+                description: "my_book",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ my.gif, my.jpg, my.png, MY.JPG, MY.Jpg,
+              http://a.b.c/x/y/z/my.gif }
+    bad = %w{ my.doc, my.gif/more, my.gif.more }
+
+    ok.each do |name|
+      assert new_product(name).valid?,
+             "#{name}  shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_product(name).invalid?,
+             "#{name}  shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new( title: products(:ruby).title,
+                           description: "about",
+                           price: 1,
+                           image_url: "my.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+    # OR
+    # assert_equal [I18n.translate('activerecord.errors.messages.taken')], product.errors[:title]
+  end
 end
